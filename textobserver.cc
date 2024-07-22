602c6837fa3ad1aa2eab2623d1e05f74274e5898
Jane He <j33he@uwaterloo.ca>
2024-07-21 20:28:06 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/main' into pieces""
    This reverts commit 6b10c6a5003e9cb3bbde81459ddf8cdce92a8ef7.

---
+++ textobserver.cc
@@ -0,0 +1,49 @@
+#include "textobserver.h"
+#include <string>
+
+TextObserver::TextObserver(Board *sub): subject{sub}{
+    subject->attach(this);
+}
+
+TextObserver::~TextObserver(){
+    subject->detach(this);
+}
+void printCurrState(Board *sub) { // print the board out
+    for(int i = 0; i < 8; i++){
+        out << 8 - i << " ";
+        for(int j = 0; j < 8; i++){
+            out << subject->getTile(i,j);
+        }
+        out << endl;
+    }
+    out << "\n  abcdefgh"<< endl;
+    
+}
+
+void declareState(Board *sub){ // check for check, checkmate, stalemate
+    if(subject->kingsOnly()){
+        out << "Insufficint Material." << endl;
+        out << "DRAW!" << endl;
+    }else if(subject->inCheck("WHITE")){
+        if(subject->inCheckmate("WHITE")){
+            out << "Checkmate! Black wins!" << endl;
+        } 
+        out << "White is in check."
+    } else if(subject->inCheck("BLACK")){
+        if(subject->inCheckmate("BLACK")){
+            out << "Checkmate! White wins!" << endl;
+        } 
+        out << "Black is in check."
+    }else if(subject->inStalemate("WHITE") || subject->inStalemate("BLACK")){
+        out << "Stalemate!" << endl;
+    } else if (subject->resigned("WHITE")){
+        out << "Black wins!" << endl;
+    } else if(subject->resigned("BLACK")){
+        out << "White wins!" << endl;
+    }
+}
+
+void TextObserver::notify() override{
+    printCurrState(subject);
+    declareState(subject);
+}
