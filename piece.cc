cea248b12f9dff93b44ae16d62ca19d8bfe6a02c
Jane He <j33he@uwaterloo.ca>
2024-07-21 20:34:53 -0400

    undoing merge for pr

--- piece.cc
+++
@@ -1,4 +0,0 @@
-#include "piece.h"
-
-Piece::Piece(Tile* position, bool hasMoved, const std::string& colour, const std::string& name)
-    : position(position), hasMoved(hasMoved), colour(colour), name(name) {}
