34ae3aac2605654e441e980c5e06b71d26f3b297
Jane He <j33he@uwaterloo.ca>
2024-07-21 21:52:12 -0400

    getting up to date

---
+++ observer.h
@@ -0,0 +1,12 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+class Subject;
+using namespace std;
+
+class Observer {
+    public:
+    virtual void notify() = 0;
+    virtual ~Observer() = default;
+};
+
+#endif
