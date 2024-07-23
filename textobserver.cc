63f557ebbeaf02a1c380d4e6fa09ff4de26794a8
d9joseph <d9joseph@uwaterloo.ca>
2024-07-23 15:51:46 -0400

    slight tweak to printCurrState

--- textobserver.cc
+++ textobserver.cc
@@ -12,7 +12,7 @@ void printCurrState(Board *sub) { // print the board out
     for(int i = 0; i < 8; i++){
         out << 8 - i << " ";
         for(int j = 0; j < 8; i++){
-            out << subject->getTile(i,j);
+            out << subject->board[i][j]; /// not coorrect snytax
         }
         out << endl;
     }
