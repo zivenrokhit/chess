485f8ad5dc0ef49f22286fd17c9913f11a959daf
d9joseph <d9joseph@uwaterloo.ca>
2024-07-24 15:10:33 -0400

    king,pawn,knight done partially

--- Pieces/piece.cc
+++ Pieces/piece.cc
@@ -1,4 +1,4 @@
 #include "piece.h"
 
-Piece::Piece(string currPos, bool hasMoved, const string& colour, const char& symbol)
-    : currPos(currPos), hasMoved(hasMoved), colour(colour), symbol(symbol) {}
+Piece::Piece(int row, int col, Board *board,const string& colour, const char& symbol)
+    :  row(row), col(col), board(board), colour(colour), symbol(symbol) {}
