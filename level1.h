62c5ff1712cde3fd1af9dd60cb5e5a7db26ae5a7
d9joseph <d9joseph@uwaterloo.ca>
2024-07-25 20:47:08 -0400

    level 1 added

--- level1.h
+++ level1.h
@@ -1,12 +1,18 @@
 #ifndef Level1_H
 #define Level1_H
-#include <string>
 #include "computer.h"
+#include "board.h"
+#include <utility>
+#include <cstdlib>
+#include <string>
 
+using namespace std;
 class Level1: public Computer {
     public:
-        Level1(const string &, Game *);
-        virtual ~Level1() override;
+    Level1(Game *game);
+    ~Level1()= default;
+    pair<int,int> getRandomMove(const vector<pair<int,int>> vecEndPos);
+    vector<pair<int,int>> Level1::getRandomPiece(bool isBlackTurn) // picks random piece, invokes getrandom move
 };
 
 #endif
